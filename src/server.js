15310b0bc48c39802b342ba520f1f93f5b81f082
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-07-18 20:09:55 +0900

    ga

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(PORT, HOST);
+console.info(`Running on http://${HOST}:${PORT}`);
